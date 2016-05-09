ca3d47c0413163672acb4acb633678da552b2183
Matthew J. Lenzo <matthew.lenzo@gmail.com>
2016-05-08 20:26:09 -0400

    Use gitconfig-local for http configs

--- scripts/getcerts.sh
+++ scripts/getcerts.sh
@@ -4,5 +4,3 @@ wget http://dodpki.c3pki.chamb.disa.mil/rel3_dodroot_2048.p7b
 mkdir -p $HOME/.ssh
 openssl pkcs7 -inform DER -outform PEM -in rel3_dodroot_2048.p7b \
      -print_certs > $HOME/.ssh/rel3_dodroot_2048.crt
-git config --global http.sslVerify true
-git config --global http.sslCAInfo $HOME/.ssh/rel3_dodroot_2048.crt
