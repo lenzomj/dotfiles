24c8d1b6061b26ab43fce1b01fceefb47320293c
Matthew J. Lenzo <matthew.lenzo@gmail.com>
2020-07-18 14:46:22 -0400

    Create install script

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -e
+
+DOTFILES_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PREFIX="$1"
+
+if [[ -z "$PREFIX" ]]; then
+  printf '%s\n' \
+    "usage: $0 <prefix>" \
+    "  e.g. $0 \$HOME" >&2
+  exit 1
+fi
+
+if [[ -d "${DOTFILES_ROOT}/.git" ]]; then
+  pushd "${DOTFILES_ROOT}" &> /dev/null;
+    if [[ -z "$(git remote | grep upstream)" ]]; then
+      git remote add upstream "git@github.com:lenzomj/dotfiles.git"
+    fi
+  popd &> /dev/null;
+fi
+
+syminstall () {
+  local _source="$1"
+  local _target=".${_source#*.}"
+
+  if [[ -L "${PREFIX}/${_target}" ]]; then
+    ln -sfn "${DOTFILES_ROOT}/${_source}" "${PREFIX}/${_target}"
+  else
+    ln -sb -S ".old" "${DOTFILES_ROOT}/${_source}" "${PREFIX}/${_target}"
+  fi
+}
+
+syminstall common.bash_profile
+syminstall common.bashrc
+syminstall common.gitconfig
+syminstall common.inputrc
+syminstall common.tmux.conf
+syminstall common.vimrc
+syminstall common.vim
+
+if [[ `command -v vim` ]]; then
+  vim -E -s -u "${PREFIX}/.vimrc" +PlugInstall +qall
+fi
+
--- scripts/getcerts.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-wget http://dodpki.c3pki.chamb.disa.mil/rel3_dodroot_2048.p7b
-mkdir -p $HOME/.ssh
-openssl pkcs7 -inform DER -outform PEM -in rel3_dodroot_2048.p7b \
-     -print_certs > $HOME/.ssh/rel3_dodroot_2048.crt
