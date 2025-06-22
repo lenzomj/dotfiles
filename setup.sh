f8c5eadd3127efaf6be354f83d78db7318e85fc8
Matthew J. Lenzo <matthew.lenzo@gmail.com>
2025-06-21 21:20:12 -0400

    Add some color to setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ set -e
 DOTFILES_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 UNINSTALL=0
 
+# Load common script functions and variables
+source "${DOTFILES_ROOT}/tools/common.sh"
+
 show_help() {
   echo "Usage: setup [options] <prefix>"
   echo
@@ -23,16 +26,16 @@ syminstall () {
   local _prefix="${2}"
   local _target="${3}"
 
-  echo "install: Creating link ${_prefix}/${_target}"
   if [ -L "${_prefix}/${_target}" ]; then
     ln -sfn "${DOTFILES_ROOT}/${_source}" "${_prefix}/${_target}"
   else
     if [ -f "${_prefix}/${_target}" ]; then
-      echo "install: Creating backup of existing dotfile ${_prefix}/${_target}"
       mv "${_prefix}/${_target}" "${_prefix}/${_target}.old"
+      print_success "Create backup of ${_prefix}/${_target}"
     fi
     ln -s "${DOTFILES_ROOT}/${_source}" "${_prefix}/${_target}"
   fi
+  print_success "Create link ${_prefix}/${_target}"
 }
 
 # Removes symlinks to dotfiles in the prefix directory.
@@ -42,15 +45,15 @@ symremove () {
   local _prefix="${2}"
   local _target="${3}"
 
-  echo "uninstall: Removing link ${_prefix}/${_target}"
   if [ -L "${_prefix}/${_target}" ]; then
     rm "${_prefix}/${_target}"
+    print_success "Remove link ${_prefix}/${_target}"
   fi
 
   # Restore any ".old" dotfiles
   if [ -f "${_prefix}/${_target}.old" ]; then
-    echo "uninstall: Restoring backup of existing dotfile ${_prefix}/${_target}"
     mv "${_prefix}/${_target}.old" "${_prefix}/${_target}"
+    print_success "Restore backup of ${_prefix}/${_target}"
   fi
 }
 
@@ -74,7 +77,7 @@ dotremove () {
 
 uninstall() {
   local prefix=$1
-  echo "uninstall: Uninstalling from $prefix..."
+  print_status "Uninstalling from $prefix..."
 
   # Bash
   dotremove "bash/common.bash_profile" "${prefix}" # .bash_profile
@@ -92,8 +95,8 @@ uninstall() {
   dotremove "vim/common.vim"           "${prefix}" # .vim
 
   # Neovim
-  echo "uninstall: Removing file ${prefix}/.config/nvim/init.vim"
   rm -f "${prefix}/.config/nvim/init.vim"
+  print_success "Remove ${prefix}/.config/nvim/init.vim"
 
   # zsh
   dotremove "zsh/common.zshrc"         "${prefix}" # .zshrc
@@ -104,12 +107,11 @@ uninstall() {
   # GPG
   symremove "gpg/gpg.conf" "${prefix}/.gnupg" "gpg.conf"
 
-  echo "uninstall: Complete"
 }
 
 install() {
   local prefix=$1
-  echo "install: Installing to $prefix..."
+  print_status "Installing to $prefix..."
 
   # Bash
   dotinstall "bash/common.bash_profile" "${prefix}" # .bash_profile
@@ -117,50 +119,63 @@ install() {
   dotinstall "other/common.inputrc"     "${prefix}" # .inputrc
 
   # Git
-  if command -v git >/dev/null 2>&1; then
+  if command_exists git; then
     dotinstall "git/common.gitconfig"   "${prefix}" # .gitconfig
+  else
+    print_warning "Git is not installed; skipping .gitconfig setup."
   fi
 
   # Tmux
-  if command -v tmux >/dev/null 2>&1; then
+  if command_exists tmux; then
     dotinstall "tmux/common.tmux.conf"  "${prefix}" # .tmux.conf
+  else
+    print_warning "Tmux is not installed; skipping .tmux.conf setup."
   fi
 
   # Vim
-  if command -v vim >/dev/null 2>&1; then
+  if command_exists vim; then
     dotinstall "vim/common.vimrc"       "${prefix}" # .vimrc
     dotinstall "vim/common.vim"         "${prefix}" # .vim
+  else
+    print_warning "Vim is not installed; skipping .vimrc and .vim setup."
   fi
 
   # Neovim
-  if command -v nvim >/dev/null 2>&1; then
-    echo "install: Creating file ${prefix}/.config/nvim/init.vim"
+  if command_exists nvim; then
     mkdir -p "${prefix}/.config/nvim"
     {
       echo "set runtimepath^=${prefix}/.vim runtimepath+=${prefix}/.vim/after"
       echo "let &packpath = &runtimepath"
       echo "source ${prefix}/.vimrc"
     } > "${prefix}/.config/nvim/init.vim"
+    print_success "Create file ${prefix}/.config/nvim/init.vim"
+  else
+    print_warning "Neovim is not installed; skipping init.vim setup."
   fi
 
   # zsh
-  if command -v zsh >/dev/null 2>&1; then
+  if command_exists zsh; then
     dotinstall "zsh/common.zshrc"       "${prefix}" # .zshrc
+  else
+    print_warning "Zsh is not installed; skipping .zshrc setup."
   fi
 
   # Mutt
-  if command -v mutt >/dev/null 2>&1; then
+  if command_exists mutt; then
     dotinstall "mutt/common.muttrc"     "${prefix}" # .muttrc
+  else
+    print_warning "Mutt is not installed; skipping .muttrc setup."
   fi
 
   # GPG
-  if command -v gpg >/dev/null 2>&1; then
+  if command_exists gpg; then
     mkdir -p "${prefix}/.gnupg"
     chmod 700 "${prefix}/.gnupg"
     syminstall "gpg/gpg.conf" "${prefix}/.gnupg" "gpg.conf"
+  else
+    print_warning "GPG is not installed; skipping gpg.conf setup."
   fi
 
-  echo "install: Complete"
 }
 
 # Parse options
@@ -174,7 +189,7 @@ while getopts ":hu" opt; do
       UNINSTALL=1
       ;;
     \?)
-      echo "Invalid option: -$OPTARG" >&2
+      print_error "Invalid option: -$OPTARG" >&2
       show_help
       exit 1
       ;;
@@ -186,14 +201,14 @@ shift $((OPTIND -1))
 
 # Ensure <prefix> is provided
 if [ -z "$1" ]; then
-  echo "Error: <prefix> argument is required." >&2
+  print_error "<prefix> argument is required." >&2
   show_help
   exit 1
 fi
 
 # Check if the prefix is a valid path
 if [ ! -d "$1" ]; then
-  echo "Error: '$1' is not a valid directory path." >&2
+  print_error "'$1' is not a valid directory path." >&2
   exit 1
 fi
 
